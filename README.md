797178aa594dabc0d83cfeea46c4a6f41ba52720
Bino Oommen Samuel <boommen@i-sight.com>
2017-06-07 01:38:25 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# picklist-generator
+## Description
+Helps generate picklists with rank
+
+## How to Run
+1. Copy the picklist values to the textarea separated by a new line
+2. Enter the filename (eg :- party_type.json)
+3. Click on generate button.
+4. Click on download button.
+
+## Credits
+* Bino Oommen Samuel
---
+++ common.js
@@ -0,0 +1,65 @@
+// Setting the message for mandatory
+function checkMandatory(fieldName) {
+	$("span").text("Fill " + fieldName +"!").show().fadeOut(1000);
+}
+// Generate the file
+function makeFile(text) {
+	var data = new Blob([text], {type: 'json'});
+	var textFile = window.URL.createObjectURL(data);
+	return textFile;
+};
+// On click for generate file
+$(document).ready(function() {
+	autosize(document.querySelectorAll('textarea'));
+	$("#form-submit").click(function (event) {
+		var picklistValues = $("#picklist-values").val();
+		if (picklistValues === ''){
+			checkMandatory("Picklist Values");
+			event.preventDefault();
+			return;
+		}
+		var picklistName = $("#picklist-name").val();
+		if (picklistName === ''){
+			checkMandatory("Picklist Name");
+			event.preventDefault();
+			return;
+		}
+		var values = picklistValues.split('\n');
+		if (picklistValues !== '' || picklistName !== '') {
+			var data = '[';
+			var name = picklistName.replace('.json', '');
+			for (var i = 0; i < values.length; i++) {
+				values[i] = values[i].replace('\n', '');
+				data += '\n\
+	{\n\
+		"name": "' + name + '",\n\
+		"value": "' + values[i] + '",\n\
+		"rank": "' + (i + 1) + '"\n\
+	},';
+			}
+			data = data.slice(0, -1);
+			data += '\n]'
+			$("#main-form").hide();
+			$("#picklist-download").show();
+			$("#picklist-output").val(data);
+			autosize.update($("#picklist-output"));
+		}
+	});
+
+	$("#form-back").click(function (event) {
+		$("#main-form").show();
+		$("#picklist-download").hide();
+	});
+
+	$("#form-clear").click(function (event) {
+		location.reload();
+	});
+
+	$("#picklist-file").click(function (event) {
+		var data = $("#picklist-output").val();
+		var file = makeFile(data);
+		this.href = file;
+		this.download = $("#picklist-name").val();
+	});
+
+});
