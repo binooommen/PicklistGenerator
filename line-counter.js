92128ee4c4e2645dcc805d345db9a29e06b5523b
Scott Hardy <hardy613@gmail.com>
2017-11-11 19:49:03 -0500

    optimised the for loop, and removed unneeded html and amd now generating the html I need in the counter file, removed the css as that was moved into the javascript file as well, it applies these styles based on the styles from the supplied element

--- line-counter.js
+++ line-counter.js
@@ -4,12 +4,20 @@
 //  could totaly be optimized but it is at least a start
 (function(root) {
 	function lineCounter (options) {
+		if(typeof options !== 'object' || !options.count) {
+			throw new Error("A count DOM element id must be provided.");
+		}
 		this.lineCount = 0;
-		this.textarea = document.getElementById(options.count);
-		this.numbers = document.getElementById(options.counter);
-		this.defaultCount = options.defaultCount || 10;
+		this.textareaId = options.count;
+		this.textarea = document.getElementById(this.textareaId);
+		this.numbers = document.createElement("div");
+		this.createDisplay();
+		this.defaultCount = Math.abs(options.defaultCount || 10);
+		this.paddingLeft = Math.abs(options.paddingLeft || 5);
 		this.textarea.addEventListener("keydown", this.display.bind(this), false);
 		this.textarea.addEventListener("keyup", this.display.bind(this), false);
+		this.numbersStyle = root.getComputedStyle(this.numbers, null);
+		this.textareaStyle = root.getComputedStyle(this.textarea, null);
 		this.display();
 	}
 
@@ -22,21 +30,57 @@
 
 	proto.display = function() {
 		var count = this.count();
-		if(this.lineCount === count) {
-			return false;
+		var lastElement = this.numbers.lastChild;
+		var num = lastElement ? lastElement.getAttribute('data-number') : 1;
+		for(; num <= count; num++) {
+			this.numbers.appendChild(this.createNumberDisplay(num));
 		}
-		this.numbers.innerHTML = "";
-		for(var i = 1; i <= count; i++) {
-			this.numbers.appendChild(this.createNumberDisplay(i));
-		}
+		// this might be able to be moved, but its good for now
+		this.textarea.style.paddingLeft =
+			parseInt(this.numbersStyle.width) + this.paddingLeft + 'px';
 		return this.lineCount = count;
+	};
+
+	proto.createDisplay = function() {
+		var lineCounterUi = document.createElement("div");
+		lineCounterUi.style.position = "relative";
+		var clone = this.textarea.cloneNode();
+		clone.classList.add(this.textareaId);
+		lineCounterUi.appendChild(clone);
+		this.addNumberStyles();
+		lineCounterUi.prepend(this.numbers);
+		this.textarea.parentNode.replaceChild(lineCounterUi, this.textarea);
+		return this.textarea = lineCounterUi.getElementsByClassName(this.textareaId)[0];
 	};
 
 	proto.createNumberDisplay = function (num) {
 		var span = document.createElement("span");
 		span.className = "number";
+		span.id = "number_" + num;
 		span.innerText = num;
+		span.setAttribute('data-number', num);
+		span.style.display = "block";
+		span.style.lineHeight = this.textareaStyle.lineHeight;
+		span.style.fontSize = this.textareaStyle.fontSize;
+		span.style.textAlign = "center";
+		span.style.padding = "0 .4em";
 		return span;
+	};
+
+	proto.addNumberStyles = function() {
+		this.numbers.style.height = "100%";
+		this.numbers.style.position = "absolute";
+		this.numbers.style.top = 0;
+		this.numbers.style.bottom =  0;
+		this.numbers.style.left = 0;
+		this.numbers.style.padding =  0;
+		this.numbers.style.marginRight = "5px";
+		this.numbers.style.borderRight = "1px solid #9d9d9d";
+		this.numbers.style.overflow = "hidden";
+		this.numbers.style.display = "inline-block";
+		this.numbers.style.color = "#9d9d9d";
+		this.numbers.style.backgroundColor = "#ccc";
+		this.numbers.style.zIndex =  1;	
 	};
 
 	return root.lineCounter = lineCounter;
