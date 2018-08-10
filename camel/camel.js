ed73c611afbe27cc8094b6805b8225e519c6491b
Bino Oommen Samuel <boommen@i-sight.com>
2018-08-10 12:51:03 -0400

    [PG-10] Fix intend

--- camel/camel.js
+++ camel/camel.js
@@ -6,7 +6,7 @@ function camelize(str) {
 	return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
 	  return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
 	}).replace(/\s+/g, '');
-  }
+}
 // On click for generate file
 $(document).ready(function() {
 	autosize(document.querySelectorAll('textarea'));
