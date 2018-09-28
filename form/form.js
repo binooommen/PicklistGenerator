4c0a2829632e855065e904a72d474c45273007fe
Bino Oommen Samuel <boommen@i-sight.com>
2018-09-28 10:17:14 -0400

    [PG-11] Added form generator

---
+++ form/form.js
@@ -0,0 +1,60 @@
+// Setting the message for mandatory
+function checkMandatory(msg) {
+	$("#error-msg-span").text(msg).show().fadeOut(1000);
+}
+// Generate the file
+function makeFile(text) {
+	var data = new Blob([text], {type: 'json'});
+	var textFile = window.URL.createObjectURL(data);
+	return textFile;
+};
+// On click for generate file
+$(document).ready(function() {
+	autosize(document.querySelectorAll('textarea'));
+	$("#form-submit").click(function (event) {
+		var fieldValues = $("#field-values").val();
+		if (fieldValues === ''){
+			checkMandatory("Fill Field Values!");
+			event.preventDefault();
+			return;
+		}
+		var values = fieldValues.split('\n');
+		if (fieldValues !== '') {
+			var data = '[';
+			for (var i = 0; i < values.length; i++) {
+				values[i] = values[i].replace('\n', '').trim();
+				data += '\n\
+	{\n\
+		field: \'' + values[i] + '\'\
+\n	},';
+			}
+			data = data.slice(0, -1);
+			data += '\n]'
+			$("#main-form").hide();
+			$("#form-download").show();
+			$("#form-output").val(data);
+			autosize.update($("#form-output"));
+		}
+	});
+
+	$("#form-back").click(function (event) {
+		$("#main-form").show();
+		$("#form-download").hide();
+	});
+
+	$("#new-form").click(function (event) {
+		location.reload();
+	});
+
+	$("#form-clear").click(function (event) {
+		location.reload();
+	});
+
+	$("#form-file").click(function (event) {
+		var data = $("#form-output").val();
+		var file = makeFile(data);
+		this.href = file;
+		this.download = 'test.js';
+	});
+
+});
